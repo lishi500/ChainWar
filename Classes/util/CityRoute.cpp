b251d6c5376014984cbe2ea5f3ee6139739def30
shli <shli@tripadvisor.com>
2018-08-22 15:11:12 +1000

    push

--- Classes/util/CityRoute.cpp
+++ Classes/util/CityRoute.cpp
@@ -2,7 +2,11 @@
 
 
 vector<City*> CityRoute::convertToCityList(vector<CityRoute::CityWraper*> cityWrapperList) {
-
+    vector<City*> citys;
+    for (size_t i = 0; i < cityWrapperList.size(); i++) {
+        citys.push_back(cityWrapperList[i]->current);
+    }
+    return citys;
 }
 
 vector<CityRoute::CityWraper*> CityRoute::convertToCityWraperList(vector<City*> cityList){
